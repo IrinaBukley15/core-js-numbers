cc0457bdca6bc1859fb354e604d9de3fb178a50e
Irina <irenserge@mail.ru>
2024-11-09 23:05:32 +0300

    feat: 2 tasks done

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -177,8 +177,11 @@ function getParallelepipedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  if (pow === 0) {
+    return num;
+  }
+  return Math.round(num / 10 ** pow) * 10 ** pow;
 }
 
 /**
@@ -198,8 +201,17 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n === 1) {
+    return true;
+  }
+  const temp = Math.sqrt(n);
+  for (let i = 2; i <= temp; i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return n > 1;
 }
 
 /**
